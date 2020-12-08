58129cb87a02f2d71115f693d16e293e96b2d98e
Rohit <dasurohit123@gmail.com>
2020-12-08 14:28:34 +0530

    made application modular

--- src/main.js
+++ src/main.js
@@ -2,32 +2,27 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 
-import result from './components/Result'
-import home from './components/Home'
-
 import ApolloClient from 'apollo-boost'
 
 Vue.config.productionTip = false;
 
-import { MdButton, MdToolbar, MdCard, MdField, MdProgress} from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
+import VueMaterial from 'vue-material'
+
 import Chartkick from 'vue-chartkick';
 import Chart from 'chart.js';
-import VueApollo from 'vue-apollo'
 
+import VueApollo from 'vue-apollo'
 const apolloClient = new ApolloClient({
   uri: 'https://assignment-rewoke.herokuapp.com/graphql'
 })
 
+import { routes } from "./routes";
+
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
 })
-
-const routes = [
-  { path: '', component: home },
-  { path: '/result', component: result }
-]
 
 const router = new VueRouter({
   mode: 'history',
@@ -36,11 +31,7 @@ const router = new VueRouter({
 
 Vue.use(VueApollo);
 Vue.use(Chartkick.use(Chart));
-Vue.use(MdButton);
-Vue.use(MdToolbar);
-Vue.use(MdCard);
-Vue.use(MdProgress);
-Vue.use(MdField);
+Vue.use(VueMaterial)
 Vue.use(VueRouter);
 
 new Vue({
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import result from './components/Result'
+import home from './components/Home'
+
+export const routes = [
+    { path: '', component: home },
+    { path: '/result', component: result }
+]
